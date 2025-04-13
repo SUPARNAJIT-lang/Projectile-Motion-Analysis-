91a717eb66630718270cfded218c9bb034b1ea96
SUPARNAJIT-lang <suparnajit.study@gmail.com>
2025-04-13 14:50:02 +0530

    Add files via upload

---
+++ Projectile.py
@@ -0,0 +1,42 @@
+#Taking input from user for the initial velocity and angle of projection
+import math 
+import matplotlib.pyplot as plt
+import numpy as np
+
+print("Welcome to the projectile motion simulation!")
+
+vi=float(input("Enter the initial velocity (in m/s): "))
+h=float(input("From which hight do you want to launch?"))
+a=float(input("Enter the angle of projection (in degrees): "))
+#Converting angle to radians
+a=math.radians(a)
+
+def pm_e(m,n): #projectile motion elements
+
+    g=9.81
+    vx=m*np.cos(n)
+    vy=m*np.sin(n)
+
+    t=(vy)/g
+    T=t+(math.sqrt((vy**2)+2*g*h))
+    return vx,vy,t,T,g
+(vx,vy,t,T,g)=pm_e(vi,a)
+
+def rp(a,b,c,d,a1,v1,g1,h1):
+    r=float(a*d)
+    x=np.linspace(0,r,1000)
+    y1=(x*np.tan(a1))-((0.5*(g1)*(x**2))/((v1**2)*((np.cos(a1))**2)))
+    y=y1+h1 
+
+    l=(v1**2)/(2*g1)
+
+    plt.plot(x,y)
+    plt.xlim(0,r)
+    plt.ylim(0,(h1+l))
+    plt.grid(True)
+    plt.savefig("boom.pdf")
+    print("The range of the projectile is:",r)
+
+rp(vx,vy,t,T,a,vi,g,h)
+
+
